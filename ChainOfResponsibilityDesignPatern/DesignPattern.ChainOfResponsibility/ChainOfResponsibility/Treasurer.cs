bc9503879eed9f06527c09cce0d831e8a34302d2
FerhanAbaci <ferhanabaci@hotmail.com>
2023-05-11 00:00:18 +0300

    Add project files.

---
+++ ChainOfResponsibilityDesignPatern/DesignPattern.ChainOfResponsibility/ChainOfResponsibility/Treasurer.cs
@@ -0,0 +1,38 @@
+﻿using DesignPattern.ChainOfResponsibility.DAL;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class Treasurer : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if(req.Amount <= 100000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Veznadar - Ayşe ÇINAR";
+                customerProcess.Description = "Para çekme işlemi gerçekleşti";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else if(NextApprover  != null)
+            {
+                Context context1 = new Context();
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Veznadar - Ayşe ÇINAR";
+                customerProcess.Description = "Para çekme işlemi vezdanarın günlük ödeyecegi tutarı aştıgı içi şube müdür yardımcısına yönlendirildi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                NextApprover.ProcessRequest(req);
+
+
+
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibilityDesignPatern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=ApiDb;User Id=SA;Password=S1611n2301T");
+        }
+
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
+ 
