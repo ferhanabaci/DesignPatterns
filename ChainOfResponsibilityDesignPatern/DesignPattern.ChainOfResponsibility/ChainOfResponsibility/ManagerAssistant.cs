0b2ddec4fac521e304fb1ca023810c0c67275f78
FerhanAbaci <ferhanabaci@hotmail.com>
2023-05-11 00:05:19 +0300

    Development

---
+++ ChainOfResponsibilityDesignPatern/DesignPattern.ChainOfResponsibility/ChainOfResponsibility/ManagerAssistant.cs
@@ -0,0 +1,38 @@
+﻿using DesignPattern.ChainOfResponsibility.DAL;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ManagerAssistant : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if (req.Amount <= 150000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Şube müdür yardımıcısı - Melike ÇINAR";
+                customerProcess.Description = "Para çekme işlemi gerçekleşti";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else if (NextApprover != null)
+            {
+                Context context1 = new Context();
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Şube müdür yardımıcısı - Melike ÇINAR";
+                customerProcess.Description = "Para çekme işlemi vezdanarın günlük ödeyecegi tutarı aştıgı içi şube müdürüne yönlendirildi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                NextApprover.ProcessRequest(req);
+
+
+
+            }
+        }
+    }
+}
