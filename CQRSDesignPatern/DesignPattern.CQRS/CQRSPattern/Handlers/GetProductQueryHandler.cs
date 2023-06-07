83ce7a56cdb3ea97c6f102f1a7cf08880bc76024
FerhanAbaci <ferhanabaci@hotmail.com>
2023-06-08 00:07:44 +0300

    The project has been registered in the repo. Developments continue

---
+++ CQRSDesignPatern/DesignPattern.CQRS/CQRSPattern/Handlers/GetProductQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.CQRS.CQRSPattern.Results;
+using DesignPattern.CQRS.DAL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPattern.CQRS.CQRSPattern.Handlers
+{
+    public class GetProductQueryHandler
+    {
+        private readonly Context _context;
+        public GetProductQueryHandler() { _context = new Context(); }
+
+        public List<GetProductQueryResult> Handle()
+        {
+            var values = _context.Products.Select(x => new GetProductQueryResult
+            {
+
+                ID = x.ProductId,
+                Price = x.Price,
+                ProductName = x.Name,
+                Stock = x.Stock,
+
+            }).ToList();
+            return values;
+        }
+    }
+}
---
+++ CQRSDesignPatern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DesignPattern2;User Id=SA;Password=S1611n2301T");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
