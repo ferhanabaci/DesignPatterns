fcd0cbac13768884293111c55810361d796e39ee
FerhanAbaci <ferhanabacii@gmail.com>
2023-06-20 20:14:15 +0300

    Paketler yüklendi, DAL klasörü oluşturuldu ve içine context sınıfı ile sql baglatısı yapıldı. Mİgration oluşturuldu

---
+++ MediatorDesignPattern/DesignPattern.Meditor/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Meditor.DAL
+{
+    public class Context: DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DesignPattern3;User Id=SA;Password=S1611n2301T");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
