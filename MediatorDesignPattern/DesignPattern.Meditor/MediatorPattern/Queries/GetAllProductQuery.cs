0663dcf767137cfb2b9876acb4b1acae6981d539
FerhanAbaci <ferhanabacii@gmail.com>
2023-06-20 21:02:01 +0300

    Result Query and Handler Class added

---
+++ MediatorDesignPattern/DesignPattern.Meditor/MediatorPattern/Queries/GetAllProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Meditor.MediatorPattern.Results;
+using MediatR;
+
+namespace DesignPattern.Meditor.MediatorPattern.Queries
+{
+    public class GetAllProductQuery : IRequest<List<GetAllProductQueryResult>> 
+    {
+    }
+}
